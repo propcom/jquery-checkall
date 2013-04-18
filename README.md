034c6e7b53417d50b1373b9099dcfa2148597c0d
Alastair McGowan-Douglas <alastair@propcom.co.uk>
2013-04-18 18:15:28 +0100

    It's a start

---
+++ README.md
@@ -0,0 +1,42 @@
+# Check-all - a jQuery plugin
+
+This is a simple jQuery plugin - like all the good ones - that associates
+checkboxes with their 'check all' checkbox.
+
+Simply select the 'check all' input and specify a selector or function for the
+ones affected by it.
+
+    $('.check-all').checkAll();
+
+The plugin takes the `rel` attribute from the selected item. Alternatively you
+may provide the `rel` option.
+
+For styling purposes the check-all box is given a class 'checked' or
+'part-checked' or neither. Since 'part-checked' is not an HTML concept the
+class is provided for you; and for consistency the 'checked' class is also
+provided.
+
+An option `onChange` can be given a function to call whenever the state of the
+check-all checkbox changes; from checked to unchecked or from either to
+part-checked. Part-checked to part-checked is not considered a change, even if
+the set of checked associated items changes.
+
+    $('.check-all').checkAll({
+		rel: '.checkme',
+		onChange: function(elem) {
+			if (elem.hasClass('part-checked')) {
+				elem.parent().addClass('part-checked');
+			}
+			else {
+				elem.parent().removeClass('part-checked');
+			}
+		}
+	});
+
+## Bugs
+
+If any of your checkboxes are disabled they are still affected by the check-all
+checkbox.
+
+If all of your checkboxes are already checked on load it doesn't check the
+checkbox.
---
+++ jquery.checkall.js
@@ -0,0 +1,63 @@
+(function($){
+	$.fn.checkAll = function(options){
+		var $elem = this;
+
+		if ($elem.length > 1) {
+			$elem.each(function() {
+				$(this).checkAll(options);
+			});
+
+			return;
+		}
+
+		var defaults = {
+			rel: $elem.attr('rel')
+		};
+		options = $.extend({}, defaults, options);
+
+		var $related;
+		if (typeof options.rel == 'function') {
+			$related = options.rel($elem);
+		}
+		else {
+			$related = $(options.rel);
+		}
+
+		// FIXME: I think this will go wrong in IE so we should store this truth value separately.
+		$elem.change(function(){
+			var c = $elem.is(':checked');
+			$related.prop('checked', c);
+
+			if (c) {
+				$elem.removeClass('part-checked').addClass('checked');
+			}
+			else {
+				$elem.removeClass('part-checked').removeClass('checked');
+			}
+
+			options.onChange && options.onChange($elem);
+		});
+
+		$related.change(function(){
+			if ($related.filter(':checked').length == $related.length) {
+				$elem.prop('checked', true);
+				$elem.removeClass('part-checked').addClass('checked');
+
+				options.onChange && options.onChange($elem);
+			}
+			else if($related.not(':checked').length == $related.length) {
+				$elem.prop('checked', false);
+				$elem.removeClass('part-checked').removeClass('checked');
+
+				options.onChange && options.onChange($elem);
+			}
+			else {
+				// Part-checked -> part-checked is not a change, so test.
+				if (! $elem.hasClass('part-checked')) {
+					$elem.addClass('part-checked').removeClass('checked').prop('checked', false);
+					options.onChange && options.onChange($elem);
+				}
+			}
+		});
+	};
+})(jQuery);
